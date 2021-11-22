7ac1b73d96bd72cad46c7815844eadd7611d89f7
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2021-11-22 15:13:38 +0800

    Uupdate homepage.
    Almost complete the homepage.

--- page/home/home.js
+++ page/home/home.js
@@ -40,9 +40,7 @@ let createPaginationSpans;
   paginationCircls[currentImageMinusOne].classList.add("active");
 
   // Move The images Area First Image
-  imagesAreaActiveImage.style.marginLeft = `-${
-    window.innerWidth * currentImageMinusOne
-  }px`;
+  imagesAreaActiveImage.style.marginLeft = `-${window.innerWidth * currentImageMinusOne}px`;
 })();
 
 // Remove All Active Class Function
@@ -75,37 +73,45 @@ setInterval(() => {
  *                                                                       *
  ************************************************************************/
 
-function updateSliderArrowsStatus() {
-  let cardsContainer = $("#anime-cards-ctn");
-  let containerWidth = $(".anime-card-ctn").width();
-  if (
-    $(cardsContainer).scrollLeft() + containerWidth <
-    document.querySelector(".anime-cards").scrollWidth
-  ) {
-    $("#anime-card-slide-right-ctn").addClass("active");
+function updateSliderArrowsStatus(elementPart) {
+  let cardsContainer = $(`#${elementPart}-anime-cards-ctn`);
+  let containerWidth = $(`#${elementPart}-anime-cards-ctn .anime-card-ctn`).width();
+
+  let currLocation = $(cardsContainer).scrollLeft() + containerWidth;
+  let max = document.querySelector(`#${elementPart}-anime-cards-ctn > div.anime-cards`).scrollWidth * 0.8;
+
+  console.log(currLocation);
+  console.log(max);
+  if (currLocation < max) {
+    $(`#${elementPart}-anime-card-slide-right-ctn`).addClass("active");
   } else {
-    $("#anime-card-slide-right-ctn").removeClass("active");
+    $(`#${elementPart}-anime-card-slide-right-ctn`).removeClass("active");
   }
   if ($(cardsContainer).scrollLeft() > 0) {
-    $("#anime-card-slide-left-container").addClass("active");
+    $(`#${elementPart}-anime-card-slide-left-container`).addClass("active");
   } else {
-    $("#anime-card-slide-left-container").removeClass("active");
+    $(`#${elementPart}-anime-card-slide-left-container`).removeClass("active");
   }
 }
 
+/**********************************
+ *
+ *      Top Anime Card Slider
+ *
+ **********************************/
+
 $(function () {
   // Scroll products' slider left/right
-  let div = $("#anime-cards-ctn");
-  let cardCount = 50; //$(div).find(".anime-cards").children(".anime-card").length
+  let div = $("#top-anime-cards-ctn");
   let speed = 350;
   let containerWidth = $(".anime-card-ctn").width();
   let cardWidth = window.innerWidth * 0.1 + 235;
 
-  updateSliderArrowsStatus();
+  updateSliderArrowsStatus("top");
 
   //Remove scrollbars
-  $("#anime-card-slide-right-ctn").click(function () {
-    if ($(div).scrollLeft() + containerWidth < cardCount * cardWidth) {
+  $("#top-anime-card-slide-right-ctn").click(function () {
+    if ($(div).scrollLeft() + containerWidth < $(div).find(".anime-cards").children(".anime-card").length * cardWidth) {
       $(div).animate(
         {
           scrollLeft: $(div).scrollLeft() + window.innerWidth * 0.75,
@@ -113,14 +119,14 @@ $(function () {
         {
           duration: speed,
           complete: function () {
-            setTimeout(updateSliderArrowsStatus(), 1005);
+            setTimeout(updateSliderArrowsStatus("top"), 1005);
           },
         }
       );
     }
-    updateSliderArrowsStatus();
+    updateSliderArrowsStatus("top");
   });
-  $("#anime-card-slide-left-container").click(function () {
+  $("#top-anime-card-slide-left-container").click(function () {
     if ($(div).scrollLeft() + containerWidth > containerWidth) {
       $(div).animate(
         {
@@ -129,52 +135,110 @@ $(function () {
         {
           duration: speed,
           complete: function () {
-            setTimeout(updateSliderArrowsStatus(), 1005);
+            setTimeout(updateSliderArrowsStatus("top"), 1005);
           },
         }
       );
     }
-    updateSliderArrowsStatus();
+    updateSliderArrowsStatus("top");
   });
 
   // If resize action ocurred then update the container width value
   $(window).resize(function () {
-    try {
-      containerWidth = $("#anime-cards-ctn").width();
-      updateSliderArrowsStatus();
-    } catch (error) {
-      console.log(
-        `Error occured while trying to get updated slider container width: 
-              ${error}`
+    updateSliderArrowsStatus("top");
+  });
+});
+
+/**********************************
+ *
+ *      Genre Anime Card Slider
+ *
+ **********************************/
+
+$(function () {
+  // Scroll products' slider left/right
+  let div = $("#genre-anime-cards-ctn");
+  let speed = 350;
+  let containerWidth = $(".anime-card-ctn").width();
+  let cardWidth = window.innerWidth * 0.1 + 235;
+
+  updateSliderArrowsStatus("genre");
+
+  //Remove scrollbars
+  $("#genre-anime-card-slide-right-ctn").click(function () {
+    if ($(div).scrollLeft() + containerWidth < $(div).find(".anime-cards").children(".anime-card").length * cardWidth) {
+      $(div).animate(
+        {
+          scrollLeft: $(div).scrollLeft() + window.innerWidth * 0.75,
+        },
+        {
+          duration: speed,
+          complete: function () {
+            setTimeout(updateSliderArrowsStatus("genre"), 1005);
+          },
+        }
       );
     }
+    updateSliderArrowsStatus("genre");
+  });
+  $("#genre-anime-card-slide-left-container").click(function () {
+    if ($(div).scrollLeft() + containerWidth > containerWidth) {
+      $(div).animate(
+        {
+          scrollLeft: "-=" + (cardWidth + window.innerWidth * 0.75),
+        },
+        {
+          duration: speed,
+          complete: function () {
+            setTimeout(updateSliderArrowsStatus("genre"), 1005);
+          },
+        }
+      );
+    }
+    updateSliderArrowsStatus("genre");
+  });
+
+  // If resize action ocurred then update the container width value
+  $(window).resize(function () {
+    updateSliderArrowsStatus("genre");
   });
 });
 
 /************************************************************************
  *                                                                       *
- *                             AnimeCard End                             *
+ *                        Init AnimeCard End                             *
  *                                                                       *
  ************************************************************************/
 
-function addAnimeCard(title, id, imageUrl) {
+function addAnimeCard(title, id, imageUrl, selector) {
   const card = document.createElement("div");
   card.className = "anime-card";
+
+  const phImage = document.createElement("a");
+  phImage.href = `../wiki/wiki.html?id=${id}`;
 
   const image = document.createElement("img");
   image.src = imageUrl;
   image.alt = title;
   image.style = "width: 100%";
-  card.appendChild(image);
+  image.setAttribute("value", id);
+
+  phImage.appendChild(image);
+  card.appendChild(phImage);
 
   const introduction = document.createElement("div");
   introduction.className = "anime-card-ctn";
   introduction.innerHTML = `<h4><b>${title}</b></h4>`;
 
   card.appendChild(introduction);
-  document.querySelector(".anime-cards").appendChild(card);
+  document.querySelector(selector).appendChild(card);
 }
 
+/**********************************
+ *
+ *      Top Anime Request
+ *
+ **********************************/
 (function () {
   var request = new XMLHttpRequest();
 
@@ -187,11 +251,57 @@ function addAnimeCard(title, id, imageUrl) {
       for (let i = 0; i < response.length; i++) {
         const anime = response[i];
 
-        addAnimeCard(anime["title"], anime["mal_id"], anime["image_url"]);
+        addAnimeCard(anime["title"], anime["mal_id"], anime["image_url"], "#top-anime-card-ctn .anime-cards");
       }
-      updateSliderArrowsStatus();
+      updateSliderArrowsStatus("top");
     }
   };
 
   request.send();
 })();
+
+/**********************************
+ *
+ *      Genre Anime Request
+ *
+ **********************************/
+(function () {
+  requestGenreAnime(1); // init when first run
+  const itemList = document.querySelectorAll("#genre-anime-card-ctn .subcategory ul li");
+
+  for (let i = 0; i < itemList.length; i++) {
+    const item = itemList[i];
+
+    item.onclick = function () {
+      const activeCSubCat = document.querySelector("#genre-anime-card-ctn #active-subcat p");
+
+      activeCSubCat.setAttribute("value", item.value);
+      activeCSubCat.textContent = item.textContent;
+
+      requestGenreAnime(item.value);
+    };
+  }
+})();
+
+function requestGenreAnime(id) {
+  var request = new XMLHttpRequest();
+
+  request.open("GET", `https://api.jikan.moe/v3/genre/anime/${id}`);
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      // Clear all existing item
+      document.querySelector("#genre-anime-card-ctn .anime-cards").innerHTML = "";
+      const response = JSON.parse(this.responseText)["anime"];
+
+      for (let i = 0; i < response.length; i++) {
+        const anime = response[i];
+
+        addAnimeCard(anime["title"], anime["mal_id"], anime["image_url"], "#genre-anime-card-ctn .anime-cards");
+      }
+      updateSliderArrowsStatus("genre");
+    }
+  };
+
+  request.send();
+}
