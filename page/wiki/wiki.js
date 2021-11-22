a05776a8ae96cf2d3af224e4f6d1c3bce0c561cb
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2021-11-23 01:42:22 +0800

    Update Wikipage
    Add the news article section.

--- page/wiki/wiki.js
+++ page/wiki/wiki.js
@@ -12,19 +12,18 @@ function getLinkParamValue(link, key) {
 }
 
 /**
- * Get the anime detail by the given id,
- * Then execute the given function when the operation is done.
+ * Sent request then execute the given function when the operation is done.
  *
- * @param {string} id - Id of the anime
+ * @param {string} link - Url
  * @param {function} executable - Use to executable when the ready state change to 4 (note: 4 represent the request operation have completed)
  */
-function getAnimeById(id, executable) {
+function setupRequest(link, executable) {
   const REQUEST_READY_STATE_DONE = 4;
   const HTTP_STATUS_OK = 200;
 
   const request = new XMLHttpRequest();
 
-  request.open("GET", `https://api.jikan.moe/v3/anime/${id}`);
+  request.open("GET", link);
   request.onreadystatechange = function () {
     //* Only continue when the operation is done
     if (REQUEST_READY_STATE_DONE === this.readyState) {
@@ -48,8 +47,41 @@ function getAnimeById(id, executable) {
 }
 
 /**
+ * Get the anime detail by the given id,
+ * Then execute the given function when the operation is done.
  *
- * @param {object} linkList
+ * @param {string} id - Id of the anime
+ * @param {function} executable - Use to execute when the ready state change to 4 (note: 4 represent the request operation have completed)
+ */
+function getAnimeById(id, executable) {
+  const link = `https://api.jikan.moe/v3/anime/${id}`;
+
+  setupRequest(link, executable);
+}
+
+/**
+ * Get the anime news by the give id,
+ * Then execute the given fucntino when the opration is done.
+ *
+ * @param {string} id - Id of the anime
+ * @param {function} executable - Use to execute when the ready state change to 4 (note: 4 represent the request operation have completed)
+ */
+function getAnimeNewsById(id, executable) {
+  const link = `https://api.jikan.moe/v3/anime/${id}/news`;
+
+  setupRequest(link, executable);
+}
+
+function getAnimeCharactersById(id, executable) {
+  const link = `https://api.jikan.moe/v3/anime/${id}/characters_staff`;
+
+  setupRequest(link, executable);
+}
+
+/**
+ * Add the external link to the wikipage.
+ *
+ * @param {object} linkList - A list of external link and name.
  */
 function addExternalLinkButton(linkList) {
   const wordList = ["Seize", "Go", "Click", "Snag", "Grip"];
@@ -78,6 +110,48 @@ function addExternalLinkButton(linkList) {
     innerDiv.appendChild(anchor);
     outerDiv.appendChild(innerDiv);
     placeholder.appendChild(outerDiv);
+  }
+}
+
+function addNewsCard(newsList) {
+  let count = 0;
+  const placeholder = document.querySelector("#ph-news-and-character #section-news");
+
+  //! Only add maximium 3 news article
+  for (let i = 0; i < newsList.length && count < 3; i++) {
+    const news = newsList[i];
+
+    //! Don't add the news when there is no image
+    if (null == news["image_url"]) {
+      return;
+    }
+
+    const article = document.createElement("article");
+
+    //! Handle figure image
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+    image.src = news["image_url"];
+    image.alt = news["title"];
+
+    figure.appendChild(image);
+    article.appendChild(figure);
+
+    //! Handle news
+    const header = document.createElement("header");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+
+    h3.textContent = news["title"];
+    p.textContent = news["intro"];
+
+    header.appendChild(h3);
+    header.appendChild(p);
+    article.appendChild(header);
+
+    //! Finalized
+    placeholder.appendChild(article);
+    count++;
   }
 }
 
@@ -111,4 +185,22 @@ function addExternalLinkButton(linkList) {
     //! Handle External Link
     addExternalLinkButton(response["external_links"]);
   });
+})();
+
+//* Handle News
+(function () {
+  const url = document.URL;
+  const id = getLinkParamValue(url, "id");
+
+  getAnimeNewsById(id, (response) => {
+    addNewsCard(response["articles"]);
+  });
+})();
+
+//* Handle Characters
+(function () {
+  const url = document.URL;
+  const id = getLinkParamValue(url, "id");
+
+  getAnimeCharactersById(id, (response) => {});
 })();
