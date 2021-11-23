e3d7ecca7792c78962563be5617eb44b696a7487
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2021-11-23 22:11:30 +0800

    Update and reformatted code.

--- component/navbar/navbar.js
+++ component/navbar/navbar.js
@@ -1,12 +1,10 @@
 window.onload = () => {
-    addNavBarElement();
-    addNavBarCss();
-}
+  addNavBarElement();
+  addNavBarCss();
+};
 
 function addNavBarElement() {
-    document
-        .getElementById("nav-top")
-        .innerHTML = `
+  document.getElementById("nav-top").innerHTML = `
     <div id="nav-mobile">
         <span onclick="openNav()">&#9776; &nbsp; Canime</span>
         <div id="ph-nav-mb" class="nav-mb-overlay">
@@ -51,23 +49,23 @@ function addNavBarElement() {
 }
 
 function addNavBarCss() {
-    var cssId = 'nav-mobile-css';
-    if (!document.getElementById(cssId)) {
-        var head = document.getElementsByTagName('head')[0];
-        var link = document.createElement('link');
-        link.id = cssId;
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = '../../component/navbar/navbar.css';
-        link.media = 'all';
-        head.appendChild(link);
-    }
+  var cssId = "nav-mobile-css";
+  if (!document.getElementById(cssId)) {
+    var head = document.getElementsByTagName("head")[0];
+    var link = document.createElement("link");
+    link.id = cssId;
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href = "../../component/navbar/navbar.css";
+    link.media = "all";
+    head.appendChild(link);
+  }
 }
 
 function openNav() {
-    document.getElementById("ph-nav-mb").style.height = "100%";
+  document.getElementById("ph-nav-mb").style.height = "100%";
 }
 
 function closeNav() {
-    document.getElementById("ph-nav-mb").style.height = "0%";
+  document.getElementById("ph-nav-mb").style.height = "0%";
 }
