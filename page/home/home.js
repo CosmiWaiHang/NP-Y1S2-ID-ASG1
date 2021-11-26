f476dc7f4b00a69b17c8d661c8ed0465b25dc8ba
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2021-11-27 04:54:34 +0800

    Improved overall performance and reformat, refactor of homepage codes.

--- page/home/home.js
+++ page/home/home.js
@@ -1,305 +1,215 @@
-// Images Area Images
-let imagesAreaImages = document.querySelectorAll(".ph-img-area img");
-// Images Area First Image
-let imagesAreaActiveImage = document.querySelector(".ph-img-area .activeImg");
+/**
+ * Extract the value of the parameter from the given link.
+ *
+ * @param {string} link - Link
+ * @param {string} key - Name of the parameter
+ */
+function getLinkParamValue(link, key) {
+  const url = new URL(link);
+  const value = url.searchParams.get(key);
 
-// Pagination Area
-let paginationArea = document.querySelector(".ph-pg-area");
-
-// Current Image Count
-let currentImageCount = 1;
-
-// Slider Controler Function
-let sliderController;
-// Create Pagination Spans Function
-let createPaginationSpans;
-
-// Create Pagination Spans [Circls] Function
-(function createPaginationSpans() {
-  // Loop On All The Images Slider
-  for (var i = 0; i < imagesAreaImages.length; i++) {
-    // Create Span
-    let paginationSpan = document.createElement("span");
-    // Append The Span
-    paginationArea.appendChild(paginationSpan);
-  }
-})();
-
-// Slider Controler Function
-(sliderController = function () {
-  // Get All The pagination Spans
-  let paginationCircls = document.querySelectorAll(".ph-pg-area span");
-
-  // Call Remore All Active Class Function
-  removeAllActive(paginationCircls);
-
-  // The currentImageCount Minus One
-  let currentImageMinusOne = currentImageCount - 1;
-
-  // Set Active Class On Current Pagination
-  paginationCircls[currentImageMinusOne].classList.add("active");
-
-  // Move The images Area First Image
-  imagesAreaActiveImage.style.marginLeft = `-${window.innerWidth * currentImageMinusOne}px`;
-})();
-
-// Remove All Active Class Function
-function removeAllActive(targetElement) {
-  for (var i = 0; i < targetElement.length; i++) {
-    targetElement[i].classList.remove("active");
-  }
+  return value;
 }
 
-// Move Slider Image Every 3 Second
-setInterval(() => {
-  // If The Current Image Count Not Equle imagesAreaImages.length
-  if (currentImageCount != imagesAreaImages.length) {
-    // Plus One
-    currentImageCount++;
-    // Call Function sliderController();
-    sliderController();
-  } else {
-    // else
-    // Make currentImageCount Equle 1
-    currentImageCount = 1;
-    // Call Function sliderController();
-    sliderController();
-  }
-}, 10000);
-
-/************************************************************************
- *                                                                       *
- *                             Slideshow End                             *
- *                                                                       *
- ************************************************************************/
-
-function updateSliderArrowsStatus(elementPart) {
-  let cardsContainer = $(`#${elementPart}-anime-cards-ctn`);
-  let containerWidth = $(`#${elementPart}-anime-cards-ctn .anime-card-ctn`).width();
-
-  let currLocation = $(cardsContainer).scrollLeft() + containerWidth;
-  let max = document.querySelector(`#${elementPart}-anime-cards-ctn > div.anime-cards`).scrollWidth * 0.8;
-
-  if (currLocation < max) {
-    $(`#${elementPart}-anime-card-slide-right-ctn`).addClass("active");
-  } else {
-    $(`#${elementPart}-anime-card-slide-right-ctn`).removeClass("active");
-  }
-  if ($(cardsContainer).scrollLeft() > 0) {
-    $(`#${elementPart}-anime-card-slide-left-container`).addClass("active");
-  } else {
-    $(`#${elementPart}-anime-card-slide-left-container`).removeClass("active");
-  }
-}
-
-/**********************************
+/**
+ * Sent request then execute the given function when the operation is done.
  *
- *      Top Anime Card Slider
- *
- **********************************/
+ * @param {string} link - Url
+ * @param {function} executable - Use to executable when the ready state change to 4 (note: 4 represent the request operation have completed)
+ */
+function setupRequest(link, executable) {
+  const REQUEST_READY_STATE_DONE = 4;
+  const HTTP_STATUS_OK = 200;
 
-$(function () {
-  // Scroll products' slider left/right
-  let div = $("#top-anime-cards-ctn");
-  let speed = 350;
-  let containerWidth = $(".anime-card-ctn").width();
-  let cardWidth = window.innerWidth * 0.1 + 235;
+  const request = new XMLHttpRequest();
 
-  updateSliderArrowsStatus("top");
-
-  //Remove scrollbars
-  $("#top-anime-card-slide-right-ctn").click(function () {
-    if ($(div).scrollLeft() + containerWidth < $(div).find(".anime-cards").children(".anime-card").length * cardWidth) {
-      $(div).animate(
-        {
-          scrollLeft: $(div).scrollLeft() + window.innerWidth * 0.75,
-        },
-        {
-          duration: speed,
-          complete: function () {
-            setTimeout(updateSliderArrowsStatus("top"), 1005);
-          },
-        }
-      );
-    }
-    updateSliderArrowsStatus("top");
-  });
-  $("#top-anime-card-slide-left-container").click(function () {
-    if ($(div).scrollLeft() + containerWidth > containerWidth) {
-      $(div).animate(
-        {
-          scrollLeft: "-=" + (cardWidth + window.innerWidth * 0.75),
-        },
-        {
-          duration: speed,
-          complete: function () {
-            setTimeout(updateSliderArrowsStatus("top"), 1005);
-          },
-        }
-      );
-    }
-    updateSliderArrowsStatus("top");
-  });
-
-  // If resize action ocurred then update the container width value
-  $(window).resize(function () {
-    updateSliderArrowsStatus("top");
-  });
-});
-
-/**********************************
- *
- *      Genre Anime Card Slider
- *
- **********************************/
-
-$(function () {
-  // Scroll products' slider left/right
-  let div = $("#genre-anime-cards-ctn");
-  let speed = 350;
-  let containerWidth = $(".anime-card-ctn").width();
-  let cardWidth = window.innerWidth * 0.1 + 235;
-
-  updateSliderArrowsStatus("genre");
-
-  //Remove scrollbars
-  $("#genre-anime-card-slide-right-ctn").click(function () {
-    if ($(div).scrollLeft() + containerWidth < $(div).find(".anime-cards").children(".anime-card").length * cardWidth) {
-      $(div).animate(
-        {
-          scrollLeft: $(div).scrollLeft() + window.innerWidth * 0.75,
-        },
-        {
-          duration: speed,
-          complete: function () {
-            setTimeout(updateSliderArrowsStatus("genre"), 1005);
-          },
-        }
-      );
-    }
-    updateSliderArrowsStatus("genre");
-  });
-  $("#genre-anime-card-slide-left-container").click(function () {
-    if ($(div).scrollLeft() + containerWidth > containerWidth) {
-      $(div).animate(
-        {
-          scrollLeft: "-=" + (cardWidth + window.innerWidth * 0.75),
-        },
-        {
-          duration: speed,
-          complete: function () {
-            setTimeout(updateSliderArrowsStatus("genre"), 1005);
-          },
-        }
-      );
-    }
-    updateSliderArrowsStatus("genre");
-  });
-
-  // If resize action ocurred then update the container width value
-  $(window).resize(function () {
-    updateSliderArrowsStatus("genre");
-  });
-});
-
-/************************************************************************
- *                                                                       *
- *                        Init AnimeCard End                             *
- *                                                                       *
- ************************************************************************/
-
-function addAnimeCard(title, id, imageUrl, selector) {
-  const card = document.createElement("div");
-  card.className = "anime-card";
-
-  const phImage = document.createElement("a");
-  phImage.href = `../wiki/wiki.html?id=${id}`;
-
-  const image = document.createElement("img");
-  image.src = imageUrl;
-  image.alt = title;
-  image.style = "width: 100%";
-  image.setAttribute("value", id);
-
-  phImage.appendChild(image);
-  card.appendChild(phImage);
-
-  const introduction = document.createElement("div");
-  introduction.className = "anime-card-ctn";
-  introduction.innerHTML = `<h4><b>${title}</b></h4>`;
-
-  card.appendChild(introduction);
-  document.querySelector(selector).appendChild(card);
-}
-
-/**********************************
- *
- *      Top Anime Request
- *
- **********************************/
-(function () {
-  var request = new XMLHttpRequest();
-
-  request.open("GET", "https://api.jikan.moe/v3/top/anime");
-
+  request.open("GET", link);
   request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      const response = JSON.parse(this.responseText)["top"];
-
-      for (let i = 0; i < response.length; i++) {
-        const anime = response[i];
-
-        addAnimeCard(anime["title"], anime["mal_id"], anime["image_url"], "#top-anime-card-ctn .anime-cards");
+    //* Only continue when the operation is done
+    if (REQUEST_READY_STATE_DONE === this.readyState) {
+      //! Stop when server request is not ok
+      if (HTTP_STATUS_OK != this.status) {
+        alert(`HTTP Request error with code: [${this.status}]`);
+        return;
       }
-      updateSliderArrowsStatus("top");
+
+      //* Parse the response text into json object
+      const response = JSON.parse(this.responseText);
+      //* Call the given function and pass the response to the given function
+      executable(response);
     }
   };
 
   request.send();
-})();
+}
 
-/**********************************
+/**
+ * Get the all relevant genre anime by the given genre id,
+ * Then execute the given function when the operation is done.
  *
- *      Genre Anime Request
- *
- **********************************/
-(function () {
-  requestGenreAnime(1); // init when first run
-  const itemList = document.querySelectorAll("#genre-anime-card-ctn .subcategory ul li");
+ * @param {string} id - Id of the anime
+ * @param {function} executable - Use to execute when the ready state change to 4 (note: 4 represent the request operation have completed)
+ */
+function getGenreAnimeById(id, executable) {
+  const link = `https://api.jikan.moe/v3/genre/anime/${id}`;
 
-  for (let i = 0; i < itemList.length; i++) {
-    const item = itemList[i];
+  setupRequest(link, executable);
+}
 
-    item.onclick = function () {
-      const activeCSubCat = document.querySelector("#genre-anime-card-ctn #active-subcat p");
+/*********************************************************************************************************************************
+ *                                                                                                                               *
+ *                                                     Document Object Model                                                     *
+ *                                                                                                                               *
+ *********************************************************************************************************************************/
 
-      activeCSubCat.setAttribute("value", item.value);
-      activeCSubCat.textContent = item.textContent;
+/**
+ * TL;DR; Make the slider run
+ */
+function InitSlider() {
+  let slideIndex = 0;
+  showSlides();
 
-      requestGenreAnime(item.value);
-    };
-  }
-})();
-
-function requestGenreAnime(id) {
-  var request = new XMLHttpRequest();
-
-  request.open("GET", `https://api.jikan.moe/v3/genre/anime/${id}`);
-
-  request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      // Clear all existing item
-      document.querySelector("#genre-anime-card-ctn .anime-cards").innerHTML = "";
-      const response = JSON.parse(this.responseText)["anime"];
-
-      for (let i = 0; i < response.length; i++) {
-        const anime = response[i];
-
-        addAnimeCard(anime["title"], anime["mal_id"], anime["image_url"], "#genre-anime-card-ctn .anime-cards");
-      }
-      updateSliderArrowsStatus("genre");
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("anime-img-slider");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-  };
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 10_000); // Change image every 2 seconds
+  }
+}
 
-  request.send();
+function addGenreAnime(animeList) {
+  const placeholder = document.getElementById("anime");
+
+  //! Convert 1D array to 2D array (matrix)
+  const animeMatrix = [];
+  while (animeList.length > 0) {
+    animeMatrix.push(animeList.splice(0, 20));
+  }
+  console.log(animeMatrix);
+
+  let count = 1;
+
+  for (const animeList of animeMatrix) {
+    const div = document.createElement("div");
+
+    //! Handle Heading
+    const header = document.createElement("header");
+    const h1 = document.createElement("h1");
+    h1.textContent = `Top ${count * 20}`;
+    header.appendChild(h1);
+    div.appendChild(header);
+
+    //! Handle Figure & Image
+    const figure = document.createElement("figure");
+    for (const anime of animeList) {
+      const article = document.createElement("article");
+      const a = document.createElement("a");
+      const image = document.createElement("img");
+
+      a.href = `../wiki/wiki.html?id=${anime["mal_id"]}`;
+      image.src = anime["image_url"];
+      image.alt = anime["title"];
+      a.appendChild(image);
+      article.appendChild(a);
+
+      const title = document.createElement("h3");
+      title.textContent = anime["title"];
+      article.appendChild(title);
+      figure.appendChild(article);
+    }
+
+    count += 1;
+    div.appendChild(figure);
+    placeholder.appendChild(div);
+  }
+}
+
+/*********************************************************************************************************************************
+ *                                                                                                                               *
+ *                                             Region: Run as soon as browser found                                              *
+ *                                                                                                                               *
+ *********************************************************************************************************************************/
+
+/**
+ * Add the image to the background slider and initialize the slider
+ */
+(function () {
+  const filenameArr = ["Attack On Titan.jpg", "FullMetal Alchemist.jpg", "Gintama.jpg", "Steins;Gate.jpg", "The Evening Sun.jpg"];
+  const container = document.getElementById("anime-slider");
+
+  for (const filename of filenameArr) {
+    const slider = document.createElement("figure");
+    slider.className = "anime-img-slider fade";
+
+    const image = document.createElement("img");
+    image.src = `../../asset/image/${filename}`;
+    image.alt = filename;
+
+    slider.appendChild(image);
+    container.appendChild(slider);
+  }
+
+  InitSlider();
+})();
+
+/**
+ * Get a list of anime by the genre id
+ */
+(function () {
+  const url = document.URL;
+  const genre = getLinkParamValue(url, "genre") ? getLinkParamValue(url, "genre").toLowerCase() : null;
+  let genreId = 0;
+
+  console.log(genre);
+
+  //! If the genre not found in url
+  if (null === genre) {
+    const list = document.getElementById("selector");
+    const actionGenre = list.children[0];
+
+    actionGenre.className = "active";
+  }
+  //! IF the genre found in url
+  else {
+    console.log(genre);
+    const list = document.getElementById("selector");
+    const itemArr = list.children;
+
+    //* Only get the first one the rest ignore
+    for (const item of itemArr) {
+      if (item.textContent.toLowerCase() == genre) {
+        item.className = "active";
+        break;
+      }
+    }
+  }
+
+  //TODO COMBINE THE LOGIC BELOW AND ABOVE
+  //! Get the genre id
+  const list = document.getElementById("selector");
+  const itemArr = list.children;
+
+  //* Only get the first one the rest ignore
+  for (const item of itemArr) {
+    if (item.classList.contains("active")) {
+      genreId = item.getAttribute("value");
+      break;
+    }
+  }
+
+  //!
+  getGenreAnimeById(genreId, (response) => {
+    addGenreAnime(response["anime"]);
+  });
+})();
+
+function updateGenre(genre) {
+  window.location.href = `./home.html?genre=${genre}`;
 }
